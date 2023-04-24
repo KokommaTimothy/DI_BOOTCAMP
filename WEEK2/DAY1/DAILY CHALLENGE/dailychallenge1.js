055abc36d0e8035cc3ec420a2b415a961883300d
KokommaTimothy <bt125829@gmail.com>
2023-04-24 14:42:17 +0300

    daily challenge wk2 day1

---
+++ WEEK2/DAY1/DAILY CHALLENGE/dailychallenge1.js
@@ -0,0 +1,15 @@
+const fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+        fruits.shift(0);
+        console.log(fruits)
+
+        fruits.sort()
+        console.log(fruits)
+
+        fruits.push("kiwi")
+        console.log(fruits)
+
+        fruits.splice(0, 1)
+        console.log(fruits)
+
+        fruits.reverse()
+        console.log(fruits)
