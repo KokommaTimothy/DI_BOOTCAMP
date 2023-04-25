a3da310448d3453efb187161329b3c9ebb993593
KokommaTimothy <bt125829@gmail.com>
2023-04-25 14:20:59 +0300

    DAILY CHALLENGE DAY 2

---
+++ WEEK2/DAY2/DAILY CHALLENGE/dailychallenge.js
@@ -0,0 +1,11 @@
+//DAY2 DAILY CHALLENGE
+
+let sentence = ("The course is not so bad, im enjoying it");
+ let wordNot = (sentence.substring(14))
+ console.log(wordNot)
+
+ let wordBad = (sentence.substring(21))
+ console.log(wordBad)
+
+ let newSentence = (sentence.replace("not so bad", "good"))
+ console.log(newSentence)
