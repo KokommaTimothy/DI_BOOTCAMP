f65fff96d75f5a16a588bf0417911dcac83d8f7f
KokommaTimothy <bt125829@gmail.com>
2023-04-26 21:41:37 +0300

    day3 daily challenge

---
+++ WEEK2/DAY3/DAILY CHALLENGE/index.js
@@ -0,0 +1,14 @@
+
+let stars = "";
+for (let i = 0; i < 6; i++) {
+    stars = stars + " * "
+    console.log( stars)
+    for (let i = 0; i < 4; i++) {
+        secondStars = stars + " * "
+        console.log( secondStars)
+    }
+    for (let i = 0; i < 2; i++) {
+        thirdStars = stars + " * "
+        console.log( thirdStars)
+    }
+}
